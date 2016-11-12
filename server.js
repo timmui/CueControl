459cc97a3b700aa8f14cf29255c7f21d6bb80b1d
Jack Mo <jackjingmo@gmail.com>
2016-11-12 12:43:43 -0500

    lightup main keys from green to red

--- server.js
+++ server.js
@@ -1,12 +1,53 @@
-const CueSDK = require('cue-sdk-node')
+const _ = require('lodash');
+const config = require('./config');
+const CueSDK = require('cue-sdk-node');
+const keypress = require('keypress');
+const sleep = require('sleep');
 
 var cue = new CueSDK.CueSDK();
 var isChanged = true;
 
-while(true){
-    if (isChanged){
-        isChanged = false;
+// // make `process.stdin` begin emitting "keypress" events 
+// keypress(process.stdin);
+
+// process.stdin.setRawMode(true);
+// process.stdin.setEncoding('utf8');
+// process.stdin.resume();
+
+// // listen for the "keypress" event 
+// process.stdin.on('keypress', function (ch, key) {
+//     console.log('got "keypress"', key);
+//     if (key && key.ctrl && key.name == 'c') {
+//         process.stdin.pause();
+//     }
+//     cue.set(key.name, config.colourRValue, config.colourGValue, config.colourBValue);
+//     console.log()
+// });
+
+
+while (true) {
+    if (isChanged) {
+        // isChanged = false;
         cue.clear();
-        cue.set('W', 255,255,255);
+
+        // var keys = cue.getLeds();
+
+        // _.forEach(config.keymap, (key) => {
+
+        // })
+        // _.forEach(keys, (key) => {
+        //     if (key.ledId )
+        //     cue.set(key.ledId, config.colourRValue, config.colourGValue, config.colourBValue, true);
+        //     sleep.usleep(50000);
+        //     console.log(key.ledId);
+        // })
+
+        for (var i = 0; i <= _.size(config.keymap); i++) {
+            cue.set(_.values(_.values(config.keymap)[i])[1],
+                config.colourRValue + 4*i, config.colourGValue - 2*i, config.colourBValue, true);
+            sleep.usleep(50000);
+            console.log(i);
+        }
+        cue.clear();
     }
 }
